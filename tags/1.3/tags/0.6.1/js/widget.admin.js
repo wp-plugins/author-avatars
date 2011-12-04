c005cc4604e79351eca94088b344c04be51d15aa
pbearne <pbearne@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-04 02:12:19 +0000

    deleted bad tag
    git-svn-id: https://plugins.svn.wordpress.org/author-avatars@470750 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tags/1.3/tags/0.6.1/js/widget.admin.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function() {
-	var bindEditLinkEvent = function() {
-		jQuery("ul.widget-control-list a.widget-control-edit[href*='edit=author_avatars']").bind('click', function() {
-			var p = jQuery(this).parent().parent().parent();
-			AA_init_avatarpreview(jQuery("div.avatar_size_preview", p), jQuery('input.avatar_size_input', p));
-		});
-	};
-	jQuery('ul.widget-control-list').bind('mouseover', bindEditLinkEvent);
-});
